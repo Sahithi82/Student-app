8798d7a32f52448c772f8f62eaeac1c70d056574
Sahithi Morampudi <sahithimorampudi@gmail.com>
2026-03-03 16:07:06 +0530

    updated student1 result

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from student import student
 def main():
-    student1=student("Sahithi",90)
+    student1=student("Sahithi",95)
     student1.display_result()
     student2=student("Jyothi",80)
     student2.display_result()
